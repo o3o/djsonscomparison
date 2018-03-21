94be29df9b489c89941c41cdd82d32d281e4703a
Orfeo Da Vià <orfeo.davia@microline.eu>
2018-03-21 16:39:15 +0100

    Add test

--- src/app.d
+++ src/app.d
@@ -1,11 +1,10 @@
-import std.stdio;
-
-void main() {
-   asdfSerialize();
-   vibeSerialize();
-
-   asdfDeserialize();
-   vibeDeserialize();
+version (unittest) {
+   import unit_threaded;
+} else {
+   import std.stdio;
+   void main(string[] args) {
+      writeln("USE dub test -- -d testname");
+   }
 }
 
 struct Simple {
@@ -14,30 +13,62 @@ struct Simple {
    bool done;
 }
 
-void asdfSerialize() {
+@("asdfSerialize")
+unittest {
    import asdf;
    auto s = Simple("asdf", 42, true);
    string expected = `{"name":"asdf","level":42,"done":true}`;
    assert(s.serializeToJson() == expected);
 }
 
-void asdfDeserialize() {
+@("asdfDeserialize")
+unittest {
    import asdf;
    auto s = Simple("asdf", 42, true);
    string data = `{"name":"asdf","level":42, "done":true}`;
    assert(data.deserialize!Simple == s);
 }
 
-void vibeSerialize() {
+@("vibeSerialize")
+unittest {
    import vibe.data.json;
    auto s = Simple("vibe", 42, true);
    string expected = `{"name":"vibe","level":42,"done":true}`;
    assert(s.serializeToJsonString() == expected);
 }
 
-void vibeDeserialize() {
+@("vibeDeserialize")
+unittest {
    import vibe.data.json;
    auto s = Simple("vibe", 32, true);
    string data = `{"name":"vibe","level":32,"done":true}`;
    assert(data.deserializeJson!Simple() == s);
+}
+
+@("vibeDeserializeName")
+unittest {
+import vibe.data.json;
+   struct Options {
+      @name("number_of_di") int diSize;
+      @name("connection_string") string connectionString;
+   }
+
+
+   auto s = Options( 32, "vibe");
+   string data = `{"connection_string":"vibe","number_of_di":32}`;
+   data.deserializeJson!Options.shouldEqual(s);
+}
+
+
+@("asdfDeserializeName")
+unittest {
+   import asdf;
+   struct Options {
+      @serializationKeys("number_of_di") int diSize;
+      @serializationKeys("connection_string") string connectionString;
+   }
+
+   auto s = Options( 32, "vibe");
+   string data = `{"connection_string":"vibe","number_of_di":32}`;
+   data.deserialize!Options.shouldEqual(s);
 }
